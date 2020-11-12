c7fd66c0a0d7954a3696bbcad6064e570607833f
Ching333 <b01043141646@gmail.com>
2020-11-12 15:50:23 +0800

    修改匯出excel和首頁圖表

--- src/views/dashboard/admin/components/BarChart.vue
+++ src/views/dashboard/admin/components/BarChart.vue
@@ -29,9 +29,9 @@ export default {
   data() {
     return {
       chart: null,
-      tagCountTop10:null,
-      tagCountTop10Count:[],
-      tagCountTop10Key:[]
+      tagCountTop10: null,
+      tagCountTop10Count: [],
+      tagCountTop10Key: []
     }
   },
   mounted() {
@@ -49,66 +49,66 @@ export default {
   methods: {
     initChart() {
       this.chart = echarts.init(this.$el, 'macarons')
-      get_tag_count().then(response=>{
-      this.tagCountTop10 = response.data;
-      this.tagCountTop10Count=[];
-      this.tagCountTop10Key=[];
-      this.tagCountTop10Count=this.tagCountTop10.map(item => Object.values(item)[0]);
-      this.tagCountTop10Key=this.tagCountTop10.map(item => Object.values(item)[1])
-       //console.log(Object.values(this.listUserGrowthKey))
-       const datas={key:this.tagCountTop10Key,count:this.tagCountTop10Count}
-       return datas
-      }).then(result=>{
-      this.chart.setOption({
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: { // 坐标轴指示器，坐标轴触发有效
-            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-          }
-        },
-        grid: {
-          top: 10,
-          left: '2%',
-          right: '2%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: [{
-          type: 'category',
-          data: result.key,
-          axisTick: {
-            alignWithLabel: true
-          }
-        }],
-        yAxis: [{
-          type: 'value',
-          axisTick: {
-            show: false
-          }
-        }],
-        series: [{
-          name: '本月份',
-          type: 'bar',
-          stack: 'vistors',
-          barWidth: '60%',
-          data: result.count,
-          animationDuration
-        }, {
-          name: '前月份',
-          type: 'bar',
-          stack: 'vistors',
-          barWidth: '60%',
-          data: [8,8,9,6,3,4,2,5,1,2],
-          animationDuration
-        }, {
-          name: '預估成長',
-          type: 'bar',
-          stack: 'vistors',
-          barWidth: '60%',
-          data:[6,8,5,9,7,5,2,5,1,2],
-          animationDuration
-        }]
-      })
+      get_tag_count().then(response => {
+        this.tagCountTop10 = response.data
+        this.tagCountTop10Count = []
+        this.tagCountTop10Key = []
+        this.tagCountTop10Count = this.tagCountTop10.map(item => Object.values(item)[0])
+        this.tagCountTop10Key = this.tagCountTop10.map(item => Object.values(item)[1])
+        // console.log(Object.values(this.listUserGrowthKey))
+        const datas = { key: this.tagCountTop10Key, count: this.tagCountTop10Count }
+        return datas
+      }).then(result => {
+        this.chart.setOption({
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: { // 坐标轴指示器，坐标轴触发有效
+              type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+            }
+          },
+          grid: {
+            top: 10,
+            left: '2%',
+            right: '2%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [{
+            type: 'category',
+            data: result.key.splice(0, 10),
+            axisTick: {
+              alignWithLabel: true
+            }
+          }],
+          yAxis: [{
+            type: 'value',
+            axisTick: {
+              show: false
+            }
+          }],
+          series: [{
+            name: '本月份',
+            type: 'bar',
+            stack: 'vistors',
+            barWidth: '60%',
+            data: result.count.splice(0, 10),
+            animationDuration
+          }, {
+            name: '前月份',
+            type: 'bar',
+            stack: 'vistors',
+            barWidth: '60%',
+            data: [8, 8, 9, 6, 3, 4, 2, 5, 1, 2],
+            animationDuration
+          }, {
+            name: '預估成長',
+            type: 'bar',
+            stack: 'vistors',
+            barWidth: '60%',
+            data: [6, 8, 5, 9, 7, 5, 2, 5, 1, 2],
+            animationDuration
+          }]
+        })
       })
     }
   }
--- src/views/dashboard/admin/components/PieChart.vue
+++ src/views/dashboard/admin/components/PieChart.vue
@@ -47,56 +47,56 @@ export default {
   methods: {
     initChart() {
       this.chart = echarts.init(this.$el, 'macarons')
-      get_place_tag_count().then(response=>{
-      this.placeTagCount = response.data;
-      this.placeTagCountValue=[];
-      this.placeTagCountName=[];
-      console.log(this.placeTagCount);
-      this.placeTagCount.forEach(item =>{
-      const x={value:item.count,name:item.key};
-      this.placeTagCountValue.push(x)
-      });
-   
-      console.log(this.placeTagCountValue);
-      
-      //this.placeTagCountValue=this.placeTagCount.map(item => Object.values(item)[0]);
-      this.placeTagCountName=this.placeTagCount.map(item => Object.values(item)[1]);
-      console.log(this.placeTagCountName);
-       //console.log(Object.values(this.listUserGrowthKey))
-       const datas={res:this.placeTagCountValue,name:this.placeTagCountName};
-       return datas
-          }).then(result=>{
-      this.chart.setOption({
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)'
-        },
-        legend: {
-          left: 'center',
-          bottom: '10',
-          data: result.name
-        },
-        series: [
-          {
-            name: '熱門地點前五名',
-            type: 'pie',
-            roseType: 'radius',
-            radius: [15, 95],
-            center: ['50%', '38%'],
-            data: result.res,
-            // [
-            //   { value: 320, name: 'Industries' },
-            //   { value: 240, name: 'Technology' },
-            //   { value: 149, name: 'Forex' },
-            //   { value: 100, name: 'Gold' },
-            //   { value: 59, name: 'Forecasts' }
-            // ],
-            animationEasing: 'cubicInOut',
-            animationDuration: 2600
-          }
-        ]
+      get_place_tag_count().then(response => {
+        this.placeTagCount = response.data
+        this.placeTagCountValue = []
+        this.placeTagCountName = []
+        console.log(this.placeTagCount)
+        this.placeTagCount.forEach(item => {
+          const x = { value: item.count, name: item.key }
+          this.placeTagCountValue.push(x)
+        })
+
+        console.log(this.placeTagCountValue)
+
+        // this.placeTagCountValue=this.placeTagCount.map(item => Object.values(item)[0]);
+        this.placeTagCountName = this.placeTagCount.map(item => Object.values(item)[1])
+        console.log(this.placeTagCountName)
+        // console.log(Object.values(this.listUserGrowthKey))
+        const datas = { res: this.placeTagCountValue, name: this.placeTagCountName }
+        return datas
+      }).then(result => {
+        this.chart.setOption({
+          tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+          },
+          legend: {
+            left: 'center',
+            bottom: '10',
+            data: result.name.splice(0, 5)
+          },
+          series: [
+            {
+              name: '熱門地點前五名',
+              type: 'pie',
+              roseType: 'radius',
+              radius: [15, 95],
+              center: ['50%', '38%'],
+              data: result.res.splice(0, 5),
+              // [
+              //   { value: 320, name: 'Industries' },
+              //   { value: 240, name: 'Technology' },
+              //   { value: 149, name: 'Forex' },
+              //   { value: 100, name: 'Gold' },
+              //   { value: 59, name: 'Forecasts' }
+              // ],
+              animationEasing: 'cubicInOut',
+              animationDuration: 2600
+            }
+          ]
+        })
       })
-    })
     }
   }
 }
